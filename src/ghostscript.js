ba697a021e0cf6220ed202b903f33bf6a3a6af96
Rémy MEJA <remy.meja@inist.fr>
2016-09-28 09:43:29 +0200

    add ram option

--- src/ghostscript.js
+++ src/ghostscript.js
@@ -1,59 +1,64 @@
-'use strict'
+'use strict';
 
-const child_process = require('child_process')
+const child_process = require('child_process');
 
 class Ghostscript {
   constructor() {
-    this.options = []
+    this.options = [];
     this._input = null
   }
   nopause() {
-    this.options.push('-dNOPAUSE')
+    this.options.push('-dNOPAUSE');
     return this
   }
   quiet() {
-    this.options.push('-q')
+    this.options.push('-q');
     return this
   }
   batch() {
-    this.options.push('-dBATCH')
+    this.options.push('-dBATCH');
+    return this
+  }
+  ram(size) {
+    const _size = size || 30000000;
+    this.options.push('-c "' + _size + ' setvmthreshold"');
     return this
   }
   device(device) {
-    let _device = device || 'tiff24nc'
-    this.options.push('-sDEVICE=' + _device)
+    const _device = device || 'tiff24nc';
+    this.options.push('-sDEVICE=' + _device);
     return this;
   }
   resolution(resolution) {
-    let _resolution = resolution || 300
-    this.options.push('-r' + _resolution)
+    const _resolution = resolution || 300;
+    this.options.push('-r' + _resolution);
     return this
   }
   firstPage(number) {
-    this.options.push('-dFirstPage=' + number)
+    this.options.push('-dFirstPage=' + number);
     return this
   }
   lastPage(number) {
-    this.options.push('-dLastPage=' + number)
+    this.options.push('-dLastPage=' + number);
     return this
   }
   input(pathPdf) {
-    this._input = pathPdf
+    this._input = pathPdf;
     return this
   }
   output(output) {
-    this.options.push('-sOutputFile=' + output)
+    this.options.push('-sOutputFile=' + output);
     return this
   }
   exec() {
     if (!this._input) {
       return new Error("Il manque un fichier en entrée")
     }
-    let args = this.options.concat(this._input).join(' ')
+    const args = this.options.concat(this._input).join(' ');
     return new Promise((resolve, reject) => {
       child_process.exec('gs ' + args, function(error, stdout, stderr) {
         if (error) {
-          reject(error)
+          reject(stderr)
         } else {
           resolve(stdout)
         }
@@ -62,4 +67,4 @@ class Ghostscript {
   }
 }
 
-module.exports = Ghostscript
+module.exports = Ghostscript;
