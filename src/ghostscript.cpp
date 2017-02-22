58c4d8e9378d27929ee97062b9ee40b8e6d2f6c0
Rémy MEJA <remy.meja@inist.fr>
2017-02-22 08:55:44 +0100

    adding some features

--- src/ghostscript.cpp
+++ src/ghostscript.cpp
@@ -6,72 +6,78 @@ using namespace std;
 using namespace Nan;
 using namespace v8;
 
-extern "C" {
-  int gsapi_new_instance (void **pinstance, void *caller_handle);
-  void gsapi_delete_instance (void *instance);
-  int gsapi_set_arg_encoding (void *instance, int encoding);
-  int gsapi_init_with_args (void *instance, int argc, char **argv);
-  int gsapi_exit (void *instance);
-};
-
 class Ghostscript : public AsyncWorker {
-  public: Ghostscript(Callback * callback) : AsyncWorker(callback) {}
+public:
+    Ghostscript(Callback *callback, vector<string> args) : AsyncWorker(callback), args(args) {}
 
-  // Executes in worker thread
-  void Execute() {
-    gsargv[0] = (char *) "gs";
-    gsargv[1] = (char *) "-q";
-    gsargv[2] = (char *) "-dNOPAUSE";
-    gsargv[3] = (char *) "-dBATCH";
-    gsargv[4] = (char *) "-sDEVICE=tiff24nc";
-    gsargv[5] = (char *) "-r300";
-    gsargv[6] = (char *) "-sOutputFile=test-%03d.tiff";
-    gsargv[7] = (char *) "test.pdf";
-    gsargc = 8;
+    // Executes in worker thread
+    void Execute() {
+        if (args.size() == 0) {
+            gscode = 1;
+            return;
+        }
 
-    gscode = gsapi_new_instance(&minst, NULL);
-    if (gscode < 0) return;
-    gscode = gsapi_set_arg_encoding(minst, GS_ARG_ENCODING_UTF8);
+        void *minst;
+        unsigned int gsargc = (unsigned int) args.size() + 1;
+        char *gsargv[gsargc];
+        gsargv[0] = (char *) "gs";
+        for (int unsigned i = 0; i < args.size(); i++) {
+            char *argsToChar = new char[args[i].length() + 1];
+            strcpy(argsToChar, args[i].c_str());
+            gsargv[i + 1] = argsToChar;
+        }
 
-    if (gscode == 0)
-        gscode = gsapi_init_with_args(minst, gsargc, gsargv);
-    gscode1 = gsapi_exit(minst);
+        gscode = gsapi_new_instance(&minst, NULL);
+        if (gscode < 0) return;
+        gscode = gsapi_set_arg_encoding(minst, GS_ARG_ENCODING_UTF8);
 
-    if ((gscode == 0) || (gscode == gs_error_Quit))
-        gscode = gscode1;
+        if (gscode == 0)
+            gscode = gsapi_init_with_args(minst, gsargc, gsargv);
+        gscode1 = gsapi_exit(minst);
 
-    gsapi_delete_instance(minst);
+        if ((gscode == 0) || (gscode == gs_error_Quit))
+            gscode = gscode1;
 
-  }
-  // Executes in event loop
-  void HandleOKCallback () {
-    if ((gscode == 0) || (gscode == gs_error_Quit)) {
-      Local<String> allOK = Nan::New<String>("All is ok !").ToLocalChecked();
-      Local<Value> argv[] = { Nan::Null(), allOK };
-      callback->Call(2, argv);
-    } else {
-      Local<String> error = Nan::New<String>("Something is wrong, dude !").ToLocalChecked();
-      Local<Value> argv[] = { error };
-      callback->Call(1, argv);
+        gsapi_delete_instance(minst);
+
     }
-  }
 
-  private:
-    void *minst;
+    // Executes in event loop
+    void HandleOKCallback() {
+        if ((gscode == 0) || (gscode == gs_error_Quit)) {
+            Local<String> allOK = Nan::New<String>("All is ok !").ToLocalChecked();
+            Local<Value> argv[] = {Nan::Null(), allOK};
+            callback->Call(2, argv);
+        } else {
+            Local<String> error = Nan::New<String>("Something is wrong, dude !").ToLocalChecked();
+            Local<Value> argv[] = {error};
+            callback->Call(1, argv);
+        }
+    }
+
+private:
+    vector<string> args;
     int gscode = 0;
     int gscode1;
-    char *gsargv[8];
-    int gsargc;
 };
 
 NAN_METHOD(exec) {
-  Callback *callback = new Callback(info[0].As<Function>());
-  AsyncQueueWorker(new Ghostscript(callback));
+    Local<Array> input = Local<Array>::Cast(info[0]);
+    vector<string> args;
+    if (input->IsArray()) {
+        for (int unsigned i = 0; i < input->Length(); i++) {
+            v8::String::Utf8Value val(Nan::Get(input, i).ToLocalChecked()->ToString());
+            std::string str(*val);
+            args.push_back(str);
+        }
+    }
+    Callback *callback = new Callback(info[1].As<Function>());
+    AsyncQueueWorker(new Ghostscript(callback, args));
 }
 
 NAN_MODULE_INIT(Init) {
-  Nan::Set(target, New<String>("exec").ToLocalChecked(),
-    GetFunction(New<FunctionTemplate>(exec)).ToLocalChecked());
+    Nan::Set(target, New<String>("exec").ToLocalChecked(),
+             GetFunction(New<FunctionTemplate>(exec)).ToLocalChecked());
 }
 
-NODE_MODULE(addon, Init)
+NODE_MODULE(ghostscript_js, Init)
--- test.js
+++ test.js
@@ -1,3 +1,7 @@
-/**
- * Created by meja on 21/02/17.
- */
+const gs = require('./build/Release/ghostscript_js');
+// const gs = require('./build/Debug/ghostscript_js');
+
+gs.exec(["-q", "-dNOPAUSE", "-dBATCH", "-sDEVICE=tiff24nc", "-r300", "-sOutputFile=test-%03d.tiff", "test.pdf"], (err, result) => {
+    if (err) console.log(err);
+    console.log(result)
+});
